0b5e683347a3ccb570b780e0212593e83a53ddbb
Tom Grant <thomas.grant.mail@gmail.com>
2023-11-19 17:09:54 +0000

    GB: Add ldr instruction

--- llvm/test/MC/GB/register-encoding.s
+++ llvm/test/MC/GB/register-encoding.s
@@ -44,3 +44,70 @@ xor l
 # CHECK-INST: xor a
 # CHECK: encoding: [0xaf]
 xor a
+
+# CHECK-INST: ld b, b
+# CHECK: encoding: [0x40]
+ld b,b
+# CHECK-INST: ld b, c
+# CHECK: encoding: [0x41]
+ld b,c
+# CHECK-INST: ld b, d
+# CHECK: encoding: [0x42]
+ld b,d
+# CHECK-INST: ld b, e
+# CHECK: encoding: [0x43]
+ld b,e
+# CHECK-INST: ld b, h
+# CHECK: encoding: [0x44]
+ld b,h
+# CHECK-INST: ld b, l
+# CHECK: encoding: [0x45]
+ld b,l
+# CHECK-INST: ld b, a
+# CHECK: encoding: [0x47]
+ld b,a
+
+# CHECK-INST: ld c, b
+# CHECK: encoding: [0x48]
+ld c,b
+# CHECK-INST: ld c, c
+# CHECK: encoding: [0x49]
+ld c,c
+# CHECK-INST: ld c, d
+# CHECK: encoding: [0x4a]
+ld c,d
+# CHECK-INST: ld c, e
+# CHECK: encoding: [0x4b]
+ld c,e
+# CHECK-INST: ld c, h
+# CHECK: encoding: [0x4c]
+ld c,h
+# CHECK-INST: ld c, l
+# CHECK: encoding: [0x4d]
+ld c,l
+# CHECK-INST: ld c, a
+# CHECK: encoding: [0x4f]
+ld c,a
+
+# CHECK-INST: ld b, c
+# CHECK: encoding: [0x41]
+ld b,c
+# CHECK-INST: ld d, c
+# CHECK: encoding: [0x51]
+ld d,c
+# CHECK-INST: ld h, c
+# CHECK: encoding: [0x61]
+ld h,c
+
+# CHECK-INST: ld c, c
+# CHECK: encoding: [0x49]
+ld c,c
+# CHECK-INST: ld e, c
+# CHECK: encoding: [0x59]
+ld e,c
+# CHECK-INST: ld l, c
+# CHECK: encoding: [0x69]
+ld l,c
+# CHECK-INST: ld a, c
+# CHECK: encoding: [0x79]
+ld a,c
