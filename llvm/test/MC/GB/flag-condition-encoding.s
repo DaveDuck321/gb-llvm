243a88fd7d591acb3f74f080e58a0f446f5197c8
Tom Grant <thomas.grant.mail@gmail.com>
2023-11-30 20:54:30 +0000

    GB: add ret cond parsing

---
+++ llvm/test/MC/GB/flag-condition-encoding.s
@@ -0,0 +1,15 @@
+# RUN: llvm-mc %s -triple=gb -show-encoding \
+# RUN:      | FileCheck -check-prefixes=CHECK,CHECK-INST %s
+
+# CHECK-INST: ret nz
+# CHECK: encoding: [0xc0]
+ret nz
+# CHECK-INST: ret nc
+# CHECK: encoding: [0xd0]
+ret nc
+# CHECK-INST: ret z
+# CHECK: encoding: [0xc8]
+ret z
+# CHECK-INST: ret c
+# CHECK: encoding: [0xd8]
+ret c
